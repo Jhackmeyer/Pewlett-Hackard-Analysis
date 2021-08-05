9b42de151b517202cc1bb975257db7215fe555ab
jhackmeyer <jhackmeyer@ucla.edu>
2021-08-05 16:54:38 -0700

    Adding table and csv for eligible mentors

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -20,9 +20,45 @@ INTO unique_titles
 FROM retirement_titles as rt
 ORDER BY emp_no, to_date DESC;
 
--- Table for retiring title counts
+-- Table for retiring counts by title
 SELECT COUNT(ut.title), ut.title
 INTO retiring_titles
 FROM unique_titles as ut
 GROUP BY ut.title
 ORDER BY count DESC;
+
+
+-- Table for mentorship eligibility
+SELECT DISTINCT ON(e.emp_no) e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	t.title
+INTO mentorship_eligibility
+FROM employees as e
+INNER JOIN dept_employees as de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles as t
+ON (e.emp_no = t.emp_no)
+WHERE (de.to_date = '9999-01-01')
+AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no;
+
+--Add department name to department count table
+SELECT dc.count,
+dc.dept_no,
+d.dept_name
+FROM dept_count as dc
+INNER JOIN departments as d
+ON (dc.dept_no = d.dept_no);
+
+-- Total number of employees by department
+SELECT COUNT(de.emp_no), de.dept_no, d.dept_name
+FROM dept_employees as de
+INNER JOIN departments as d
+on (de.dept_no = d.dept_no)
+GROUP BY de.dept_no, d.dept_name
+ORDER BY de.dept_no;
+
